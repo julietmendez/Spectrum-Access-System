71a685681450edda9661ab1e7bf0c4db41ad2165
Bryan CS <yanser25@gmail.com>
2022-04-19 02:21:55 +0800

    Update entrypoint.sh
    set correct common-data path on container

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,9 @@ set -eux
 # We expect a Common-Data volume to be mounted at $COMMON_DATA path
 
 if [[ -d "$COMMON_DATA" ]]; then
-    ln -s $COMMON_DATA/ned /opt/app/src/data/geo/ned
-    ln -s $COMMON_DATA/nlcd /opt/app/src/data/geo/nlcd
-    ln -s $COMMON_DATA/counties /opt/app/src/data/counties
+    ln -s $COMMON_DATA/ned /opt/app/data/geo/ned
+    ln -s $COMMON_DATA/nlcd /opt/app/data/geo/nlcd
+    ln -s $COMMON_DATA/counties /opt/app/data/counties
 else
     echo -e "$(tpud setaf 1)ERR: No Common-Data found at $COMMON_DATA\!"
     exit 1
